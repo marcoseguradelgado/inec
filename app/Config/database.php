1af82c961590bfb9e472a36a1b50d65d278d4cac
root <root@marco-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-08-15 18:14:20 -0600

    Add database file

---
+++ app/Config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'mysql_1992',
+		'database' => 'dev_inec',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}
